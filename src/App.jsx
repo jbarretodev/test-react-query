a3e3d44778a9418efdb45af66d96262f2a3c53e2
juanchaoss <jbarretodev6105@gmail.com>
2023-09-09 15:00:39 -0400

    commit initial!

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <div>App Hola</div>
+  )
+}
+
+export default App
---
+++ src/api/productsApi.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const productApi = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+export const getProducts = async () => {
+  const res = await productApi.get("/products");
+  return res.data;
+};
